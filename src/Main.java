26fad5525949b5474f9bc36332e4b258f057ad32
Dasha Kozyulya <w.vnt0d@gmail.com>
2022-10-17 22:57:07 +0500

    add file

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 6_980;
+
+        int miles = price / 20;
+        System.out.println("Бонусные мили:" + miles);
+
+
+    }
+}
